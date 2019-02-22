855616cc838f2796b9e6fd415226e6547ba882bf
Vicky Wong <vickywg998@hotmail.com>
2019-02-21 17:29:39 -0800

    imported API before mapping video list

--- bandsite2/App/src/JS/Components/HomePage.js
+++ bandsite2/App/src/JS/Components/HomePage.js
@@ -5,11 +5,33 @@ import Main from '../Main'
 import Section from '../Section'
 import cardImages from '../Components/Sidebar/cardImages'
 import CommentObjs from '../Components/Section/CommentObjs'
-// import Upload from './UploadPage/Upload'
+import axios from 'axios';
 
 import { BrowserRouter, Switch, Router, Route, Link } from "react-router-dom";
 
+const myAPI = "1a2da34e-4760-45ed-9b07-e9acc896e17f";
+const url = "https://project-2-api.herokuapp.com/videos/?api_key="+ myAPI
+
+
 class HomePage extends Component {
+  state = {
+videos: []
+  }
+
+  componentDidMount() {
+    axios
+      .get(url)
+      .then(response => {
+        console.log(response.data)
+       
+        this.setState({
+          videos: response.data
+        })
+      })
+  }
+
+
+
   render() {
     return (
       <div>
--- bandsite2/App/src/JS/Components/UploadPage/Upload.js
+++ bandsite2/App/src/JS/Components/UploadPage/Upload.js
@@ -8,27 +8,33 @@ class Upload extends React.Component {
     return (
       <div>
 
-       
         <div className="upload__container">
           <h1 className="upload__header">Upload Video</h1>
-<div className="upload__wrapper--big">
+          <div className="upload__wrapper--big">
 
-<div className="upload__wrapper--small upload__wrapper--left">
-<h5 className="videothumbnail__title">VIDEO THUMBNAIL</h5>
-<UploadPreview />
-</div>
+            <div className="upload__wrapper--small upload__wrapper--left">
+              <h5 className="videothumbnail__title">VIDEO THUMBNAIL</h5>
+              <UploadPreview />
+            </div>
 
-<div className="upload__wrapper--small upload__wrapper--right">
-<h5 className="videothumbnail__title">TITLE YOUR VIDEO</h5>
-<input type="text" name="videotitle" value="Add a title to your video" />
-<h5 className="videothumbnail__title">ADD A VIDEO DESCRIPTION</h5>
-<textarea type="text" name="videodescription" value="Add a description to your video" />
+            <div className="upload__wrapper--small upload__wrapper--right">
+              <h5 className="videothumbnail__title">TITLE YOUR VIDEO</h5>
+              <input className="videotitle__input" type="text" name="videotitle" value="Add a title to your video" />
+              <h5 className="videothumbnail__title">ADD A VIDEO DESCRIPTION</h5>
+              <textarea className="videodescription__input" type="text" name="videodescription" value="Add a description to your video" />
+            </div>
+          </div>
 
-</div>
+          <div className="publish__container">
+            <div className="publish__wrapper">
+              <h5 id="cancel__text">Cancel</h5>
+            </div>
+            <div className="publish__wrapper">
+              <button className="publish__button">PUBLISH</button>
+            </div>
 
 
-
-</div>
+          </div>
 
 
 
